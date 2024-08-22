d8996c76165b017ec54a7d41e2e68c27983c752f
hasansahin11 <qa.hasansahin@gmail.com>
2024-08-22 01:07:56 -0500

    Base Driver Class created

---
+++ src/test/java/fetchchallange/base/BaseTest.java
@@ -0,0 +1,29 @@
+package fetchchallange.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeClass
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://sdetchallenge.fetch.com/");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+}
